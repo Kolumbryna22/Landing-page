93c80c64b1294e2afce675a7f9b8106b45b773f9
Dominik Wilusz <wilusz1@o2.pl>
2018-07-09 23:51:45 +0200

    Fix some issues

--- js/script.js
+++ js/script.js
@@ -3,12 +3,10 @@
 
     window.addEventListener('scroll', function() {
         var navigate = document.getElementById('navigate');
+        navigate.style.background = 'none';
 
         if(window.scrollY > 20) {
             navigate.style.background = 'rgba(0, 0, 0, 0.6)';
-        }
-        else {
-            navigate.style.background = 'rgba(0, 0, 0, 0)';
         }
     });
 })();
