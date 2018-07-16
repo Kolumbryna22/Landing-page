6fa5a989b3647acd196fe16982d3cad9b8bb5022
Dominik Wilusz <wilusz1@o2.pl>
2018-07-16 17:23:06 +0200

    Add hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 (function() {
     'use strict';
 
+    var button = document.getElementById('hamburger');
+    var nav = document.getElementById('nav');
+    var change = false;
+
     window.addEventListener('scroll', function() {
         var navigate = document.getElementById('navigate');
         navigate.style.background = window.scrollY > 20 ? 'rgba(0, 0, 0, 0.6)' : 'none';
     });
+
+    window.addEventListener('resize', function(){
+        if (window.innerWidth > 588) {
+            nav.style.display = "block";
+            change = false;
+        }
+        else {
+            nav.style.display = "none";
+        }
+    })
+    
+    var changeVisible = function() {
+        change ? (nav.style.display = "block") : (nav.style.display = "none");
+        change = !change;
+    }
+
+    button.addEventListener('click', changeVisible);
 })();
